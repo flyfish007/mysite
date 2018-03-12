0e0f3eb51e7f2d9271a925065718c7c024e21992
guofangfang <252186302@qq.com>
2018-03-12 13:47:58 +0800

    my test

---
+++ Content/Resources/MasterPages/home_map.js
@@ -0,0 +1,17 @@
+﻿	
+			
+			$(function(){
+			var _w = parseInt($(window).width());
+			var _pc = _w / 650;
+			if(_w < 650){
+				$("area").each(function(e){
+				var _cd = $(this).attr("coords");
+				var _carr = _cd.split(",");
+				for(var i=0;i<_carr.length;i++){
+				_carr[i] = Math.ceil(_carr[i] * _pc);
+				}
+				$(this).attr("coords",_carr.join(","));
+				});
+				}
+				});
+	
---
+++ Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=636535447593011776'
+});
